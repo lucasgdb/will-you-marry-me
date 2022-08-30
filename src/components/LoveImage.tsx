7441e292aead46959a0fffd886461b59b3459ce4
lucasgdb <lucasgdbittencourt@gmail.com>
2022-08-30 16:37:38 -0300

    chore: prevent image to be draggable

--- src/components/LoveImage.tsx
+++ src/components/LoveImage.tsx
@@ -8,6 +8,7 @@ export default function LoveImage({ hidden }: LoveImageProps) {
   return (
     <img
       src={us}
+      draggable={false}
       className={`rounded transition-opacity duration-500 w-[260px] absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 ${
         hidden ? 'opacity-0' : 'opacity-100'
       }`}
